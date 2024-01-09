a03e9acc4318f3b70f0ab0880e9660cf0996ca43
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-09 07:53:46 -0500

    added nav bar

---
+++ josesmathjournal/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <Link to='/'> 
+                <div>Home</div>
+            </Link>
+            <Link to='/Functanal'>
+                <div>Functional Analysis</div>
+            </Link>
+            <Link to='/DiffGeo'>
+                <div>Differential Geometry</div>
+            </Link>
+        </div>
+    )
+}
