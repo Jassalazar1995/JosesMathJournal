49a97d31367ded7f0ed041837b7da08ff42e9c16
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-14 21:46:24 -0500

    Added some more styling.

--- josesmathjournal/src/pages/Main.jsx
+++ josesmathjournal/src/pages/Main.jsx
@@ -5,7 +5,7 @@ import YouTube from 'react-youtube';
 export default function Main() {
 
     return (
-        <div>
+        <div className="container ml-auto mr-auto mt-40 p-4">
             <h1>Main Page</h1>
         <p>Welcome to the main page. This is a website for learning advanced mathematics. Please feel free to use the pomodoro clock at the top and explore through the website. Thank you.</p>
         </div>
