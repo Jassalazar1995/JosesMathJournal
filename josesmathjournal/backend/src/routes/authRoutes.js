171408b9ea0f754ef8ca5a9c48484bb305e1e767
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-25 11:43:11 -0500

    registering on the backend now works and sends to the database.

--- josesmathjournal/backend/src/routes/authRoutes.js
+++ josesmathjournal/backend/src/routes/authRoutes.js
@@ -1,1 +1,8 @@
 // The authController.js and authRoutes.js will handle user registration, login, and authentication
+const express = require('express');
+const router = express.Router();
+const {register } = require( '../controllers/authController' )
+
+router.post('/register', register)
+
+module.exports = router
