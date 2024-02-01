9f99adef6ee8e4d6128c7958560497992a3313ee
Jose Salazar <jassalazar1995@gmail.com>
2024-02-01 09:20:52 -0500

    I think I put the wrong proxy

--- josesmathjournal/client/vite.config.js
+++ josesmathjournal/client/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({server: {
   proxy: {
-    '/auth': 'https://josemathjournalfrontend.onrender.com/',
-    '/api': 'https://josemathjournalfrontend.onrender.com/'
+    '/auth': 'https://josemathjournal.onrender.com',
+    '/api': 'https://josemathjournal.onrender.com'
     }
   },
   plugins: [react()],
