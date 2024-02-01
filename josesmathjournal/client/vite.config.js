ce5bf5c86d0e529fd8b4143074277c67f16a6eef
Jose Salazar <jassalazar1995@gmail.com>
2024-02-01 09:09:54 -0500

    changed the proxies to try to get it to connect to the backend.

--- josesmathjournal/client/vite.config.js
+++ josesmathjournal/client/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({server: {
   proxy: {
-    '/auth': 'http://localhost:5000',
-    '/api': 'http://localhost:5000'
+    '/auth': 'https://josemathjournalfrontend.onrender.com/',
+    '/api': 'https://josemathjournalfrontend.onrender.com/'
     }
   },
   plugins: [react()],
